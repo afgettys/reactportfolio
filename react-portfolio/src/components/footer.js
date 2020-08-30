b8ec29cbbec5080d01310620ae50cd927680bd16
Amanda Gettys <afgettys@gmail.com>
2020-08-30 19:38:29 -0400

    adding components

---
+++ react-portfolio/src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../styles/footer.css"
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <div class="col-md-12">
+            <p role= "img" id="mm">Amanda's React Portfolio<span role="img" aria-label="jsx-a11y/accessible-emoji">❤️</span>
+            </p>
+            <p id="date">09/2020
+            </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
